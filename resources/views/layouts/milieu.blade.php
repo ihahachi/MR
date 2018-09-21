128a2e8029372e439d081d45837788e5bfc706ba
ihahachi <iha2007hachi@gmail.com>
2018-09-21 16:07:30 +0100

    include sub-view

---
+++ resources/views/layouts/milieu.blade.php
@@ -0,0 +1,7 @@
+<select  class="form-control" name="milieu" id="milieu">
+    <option selected>Choisir...</option>
+    <option value="01">Les Oueds</option>
+    <option value="02">Les Grands Ensembles Portuaires</option>
+    <option value="03">Baies du Littoral</option>
+    <option value="04">Lacs et Chotts</option>
+</select>
---
+++ resources/views/layouts/zone.blade.php
@@ -0,0 +1,41 @@
+<select  class="form-control" name="zone" id="zone">
+    <option value="01_Macta">Macta</option>
+    <option value="01_Tafna">Tafna</option>
+    <option value="01_Chellif">Chellif</option>
+    <option value="01_Mazafran">Mazafran</option>
+    <option value="01_El Harrach">El Harrach</option>
+    <option value="01_EL Hamiz">EL Hamiz</option>
+    <option value="01_Les Issers">Les Issers</option>
+    <option value="01_Sebaou">Sebaou</option>
+    <option value="01_Soumam">Soumam</option>
+    <option value="01_Djendjen">Djendjen</option>
+    <option value="01_Saf-Saf">Saf-Saf</option>
+    <option value="01_Seybouse">Seybouse</option>
+
+    <option value="02_Ghazaouet">Ghazaouet</option>
+    <option value="02_Oran">Oran</option>
+    <option value="02_Arzew">Arzew</option>
+    <option value="02_Alger">Alger</option>
+    <option value="02_Béjaia">Béjaia</option>
+    <option value="02_Skikda">Skikda</option>
+    <option value="02_Djendjen">Djendjen</option>
+    <option value="02_Annaba">Annaba</option>
+
+    <option value="03_Les Andalouses">Les Andalouses</option>
+    <option value="03_Golf d'Arzew">Golf d'Arzew</option>
+    <option value="03_Bou Ismail">Bou Ismail</option>
+    <option value="03_Alger">Alger</option>
+    <option value="03_Béjaia">Béjaia</option>
+    <option value="03_Golf de stora">Golf de stora</option>
+    <option value="03_Chétaibi">Chétaibi</option>
+    <option value="03_Baies des corailleurs">Baies des corailleurs</option>
+
+    <option value="04_Sebkha d'Oran">Sebkha d'Oran</option>
+    <option value="04_Zehrez Gharbi">Zehrez Gharbi</option>
+    <option value="04_Chott El Hodna">Chott El Hodna</option>
+    <option value="04_Lac Réghaia">Lac Réghaia</option>
+    <option value="04_Lac Tonga">Lac Tonga</option>
+    <option value="04_Lac Oubira">Lac Oubira</option>
+    <option value="04_Lac Fetzara">Lac Fetzara</option>
+    <option value="04_Lac des Oiseaux">Lac des Oiseaux</option>
+</select>
